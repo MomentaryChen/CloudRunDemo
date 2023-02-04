5e426b1a685f4e4edc227612770b2dd29be7f964
unknown <zzser15963@gmail.com>
2023-02-04 17:57:48 +0800

    update

--- src/main/java/com/demo/CloudRunDemo/CloudRunDemoApplication.java
+++ src/main/java/com/demo/CloudRunDemo/CloudRunDemoApplication.java
@@ -1,13 +1,42 @@
 package com.demo.CloudRunDemo;
 
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.catalina.core.ApplicationContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.demo.CloudRunDemo.service.EmployeeService;
+
 
 @SpringBootApplication
 public class CloudRunDemoApplication {
+	private static final Logger logger = LogManager.getLogger(CloudRunDemoApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudRunDemoApplication.class, args);
+	@Autowired
+	EmployeeService service;
+
+	@PostConstruct
+	public void batch() {
+		logger.info("start the batch");
+		try {
+			
+			service.findAll().forEach(System.out::println);
+			TimeUnit.SECONDS.sleep(10);
+			logger.info("application cloed");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(CloudRunDemoApplication.class, args);
+		ctx.close();
+	}
 }
